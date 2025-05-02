aaddfa335430d7d8e50ae58572d74edf2db80152
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2025-05-01 23:30:18 -0300

    feat: Add name in project

---
+++ Seção 5 - DOM/DOM_saudacao_original/js/saudacao.js
@@ -0,0 +1,9 @@
+;(function () {
+  const nomeUsuario = 'Rodrigo'
+
+  //   document.querySelector('.top-bar p').textContent =
+  //     'Bem-vindo(a)' + nomeUsuario
+  const elemento = document.querySelector('.top-bar p')
+  //   elemento.textContent = elemento.textContent + nomeUsuario
+  elemento.textContent += nomeUsuario
+})()
