12579c32e24915d8b98c1d82d3bfb9beb645b23b
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2025-05-05 22:31:36 -0300

    feat: Enabling and disabling inputs

--- Seção 5 - DOM/DOM_edit_textfield_original/js/newsletter.js
+++ Seção 5 - DOM/DOM_edit_textfield_original/js/newsletter.js
@@ -1,4 +1,10 @@
-const txtEmail = document.getElementById("txtEmail")
+const txtEmail = document.getElementById('txtEmail')
 
+function editarEmail() {
+  txtEmail.disabled = false
+  txtEmail.focus()
+}
 
-
+function desabilitarEdit() {
+  txtEmail.disabled = true
+}
